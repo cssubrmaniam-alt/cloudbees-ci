4f66d00f4e176c76f9b018373679660b86d08e1f
Saravanan Subramaniam <c_ssubramaniam@cloudbees.com>
2026-05-14 13:13:36 -0500

    Complete phase 2 traditional VM base infrastructure

--- terraform/gcp-vm-traditional/main.tf
+++ terraform/gcp-vm-traditional/main.tf
@@ -1,39 +1,61 @@
 resource "google_project_service" "compute" {
-  project = var.project_id
-  service = "compute.googleapis.com"
+  project            = var.project_id
+  service            = "compute.googleapis.com"
   disable_on_destroy = false
 }
 
+resource "google_compute_subnetwork" "traditional" {
+  name          = "${var.vm_name}-subnet"
+  ip_cidr_range = "10.20.0.0/24"
+  region        = var.region
+  network       = "default"
+
+  depends_on = [google_project_service.compute]
+}
+
 resource "google_compute_firewall" "allow_jenkins_lab" {
-  name = "${var.vm_name}-allow-jenkins"
+  name    = "${var.vm_name}-allow-jenkins"
   network = "default"
-  allow { protocol = "tcp"; ports = ["22","8080","8443","50000"] }
-  source_ranges = ["0.0.0.0/0"]
-  target_tags = [var.vm_name]
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "8080", "8443", "50000"]
+  }
+
+  source_ranges = var.allowed_source_ranges
+  target_tags   = [var.vm_name]
+
   depends_on = [google_project_service.compute]
 }
 
 resource "google_compute_instance" "traditional" {
-  name = var.vm_name
+  name         = var.vm_name
   machine_type = var.machine_type
-  zone = var.zone
-  labels = var.labels
-  tags = [var.vm_name]
+  zone         = var.zone
+  labels       = var.labels
+  tags         = [var.vm_name]
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-12"
-      size = var.disk_gb
-      type = "pd-balanced"
+      image  = "debian-cloud/debian-12"
+      size   = var.disk_gb
+      type   = "pd-balanced"
       labels = var.labels
     }
   }
 
   network_interface {
-    network = "default"
+    subnetwork = google_compute_subnetwork.traditional.self_link
+
     access_config {}
   }
 
-  metadata = { enable-oslogin = "TRUE" }
-  depends_on = [google_project_service.compute]
+  metadata = {
+    enable-oslogin = "TRUE"
+  }
+
+  depends_on = [
+    google_project_service.compute,
+    google_compute_subnetwork.traditional
+  ]
 }
--- terraform/gcp-vm-traditional/variables.tf
+++ terraform/gcp-vm-traditional/variables.tf
@@ -5,3 +5,8 @@ variable "vm_name" { type = string }
 variable "machine_type" { type = string }
 variable "disk_gb" { type = number }
 variable "labels" { type = map(string) }
+
+variable "allowed_source_ranges" {
+  type        = list(string)
+  description = "CIDR ranges allowed to access SSH and Jenkins/CloudBees lab ports."
+}
